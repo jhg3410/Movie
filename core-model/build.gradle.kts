3fcdb4a770e87efeca0ae3eedf61bfcbed84ff2e
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2023-04-06 21:00:08 +0900

    Apply Type-safe project accessors and update build.gradle

--- core-model/build.gradle.kts
+++ core-model/build.gradle.kts
@@ -1,6 +1,6 @@
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
 }
 
--- core-network/build.gradle.kts
+++ core-network/build.gradle.kts
@@ -1,6 +1,7 @@
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
 }
 
 android {
@@ -34,10 +35,9 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation(projects.coreModel)
+    implementation(libs.androidx.ktx)
+
+    // test
+    testImplementation(libs.junit)
 }
