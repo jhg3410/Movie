638b242729463d05d387fd05b6450a5c82df3c9b
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2023-06-29 16:22:51 +0900

    Apply TopAppBar do not composition in DetailScreen

--- app/src/main/java/com/jik/movie/MainActivity.kt
+++ app/src/main/java/com/jik/movie/MainActivity.kt
@@ -3,19 +3,8 @@ package com.jik.movie
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.navigation.compose.rememberNavController
-import com.jik.core.designsystem.component.MovieTopAppBar
-import com.jik.core.designsystem.theme.MovieTheme
-import com.jik.movie.navigation.MovieNavHost
+import com.jik.movie.ui.MovieApp
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.*
 import kotlin.time.Duration.Companion.seconds
 
 @AndroidEntryPoint
@@ -26,30 +15,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MovieApp()
-        }
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun MovieApp() {
-    MovieTheme(
-        dynamicColor = false
-    ) {
-        val navController = rememberNavController()
-
-        val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
-
-        Scaffold(
-            modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-            topBar = {
-                MovieTopAppBar(
-                    titleRes = R.string.app_name,
-                    scrollBehavior = scrollBehavior
-                )
-            }
-        ) {
-            MovieNavHost(navController = navController, modifier = Modifier.padding(it))
         }
     }
 }
---
+++ app/src/main/java/com/jik/movie/ui/MovieApp.kt
@@ -0,0 +1,37 @@
+package com.jik.movie.ui
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import com.jik.core.designsystem.component.MovieTopAppBar
+import com.jik.core.designsystem.theme.MovieTheme
+import com.jik.core.ui.R
+import com.jik.movie.navigation.MovieNavHost
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MovieApp() {
+    MovieTheme(
+        dynamicColor = false
+    ) {
+        val appState = rememberMovieAppState()
+        val scrollBehavior = appState.topAppBarScrollBehavior
+
+        Scaffold(
+            modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+            topBar = {
+                if (appState.isTopLevelDestination) {
+                    MovieTopAppBar(
+                        titleRes = R.string.app_name,
+                        scrollBehavior = scrollBehavior
+                    )
+                }
+            }
+        ) {
+            MovieNavHost(navController = appState.navController, modifier = Modifier.padding(it))
+        }
+    }
+}
