f55bbd4f17b7e24bed2bd9718852326021236395
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2023-07-02 22:15:19 +0900

    Display content edge-to-edge in Detail Screen

--- core-designsystem/src/main/java/com/jik/core/designsystem/theme/Theme.kt
+++ core-designsystem/src/main/java/com/jik/core/designsystem/theme/Theme.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
@@ -100,7 +101,7 @@ fun MovieTheme(
         val window = (view.context as Activity).window
 
         SideEffect {
-            window.statusBarColor = colorScheme.background.toArgb()
+            window.statusBarColor = Color.Transparent.toArgb()
             window.navigationBarColor = colorScheme.background.toArgb()
             WindowCompat.getInsetsController(window, view).run {
                 isAppearanceLightStatusBars = darkTheme.not()
