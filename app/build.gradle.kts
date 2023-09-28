496233c49d9969bc0fb3fe0ce35f1d5bc59eccf7
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2023-09-28 20:49:40 +0900

    Update version to 0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 24
         targetSdk = 33
         versionCode = 1
-        versionName = "0.2.0"
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
