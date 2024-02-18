d70a53f47063f40538af19c0ca4b0c7b6496a97f
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2024-02-18 23:00:21 +0900

    lib-videoplayer apply version catalog

--- lib-videoplayer/build.gradle.kts
+++ lib-videoplayer/build.gradle.kts
@@ -55,17 +55,20 @@ chaquopy {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation(libs.androidx.ktx)
 
     // compose
     implementation(libs.compose.ui)
     implementation(libs.compose.material3)
     implementation(libs.compose.material.icons)
+
+    // test
+    testImplementation(libs.junit)
+
+    // android test
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso)
+    androidTestImplementation(libs.compose.test.junit)
 
     // exoplayer
     implementation(libs.media3.exoplayer)
