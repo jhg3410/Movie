59b0f925f39e2ca6153d38d12804bcdd86dce8f1
jhg3410 <jhg34100@naver.com>
2024-09-03 20:23:01 +0900

    version up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "0.3.0"
+        versionName = "0.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
