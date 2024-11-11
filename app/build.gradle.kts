3d3f4a4a9ec2aa92697653b584bc5093d748ab85
jhg3410 <jhg34100@naver.com>
2024-11-12 00:26:48 +0900

    versionName up(0.4.0 -> 0.4.1)
    https://github.com/chaquo/chaquopy/issues/1249

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "0.4.0"
+        versionName = "0.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
