366d626a871c800e7ecffba315493c019b5dc967
jhg3410 <jhg34100@naver.com>
2024-11-12 22:38:27 +0900

    custom Slider Thumb 구현

--- lib/videoplayer/src/main/java/com/jik/lib/videoplayer/component/slider/SliderDefaults.kt
+++ lib/videoplayer/src/main/java/com/jik/lib/videoplayer/component/slider/SliderDefaults.kt
@@ -1,20 +1,55 @@
 package com.jik.lib.videoplayer.component.slider
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.hoverable
+import androidx.compose.foundation.indication
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.SliderState
+import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 
-object MovieSliderDefaults {
+internal object MovieSliderDefaults {
+
+    @Composable
+    fun Thumb(
+        modifier: Modifier = Modifier,
+        color: Color,
+        shape: Shape = CircleShape,
+        interactionSource: MutableInteractionSource,
+    ) {
+        Spacer(
+            modifier = modifier
+                .padding(top = 2.dp)
+                .size(size = 12.dp)
+                .indication(
+                    interactionSource = interactionSource,
+                    indication = ripple(
+                        bounded = false,
+                        radius = 18.dp
+                    )
+                )
+                .hoverable(interactionSource = interactionSource)
+                .clip(shape = shape)
+                .background(color = color)
+        )
+    }
 
     @OptIn(ExperimentalMaterial3Api::class)
     @Composable
@@ -72,9 +107,26 @@ object MovieSliderDefaults {
             )
         }
     }
+
+    @Composable
+    fun colors(
+        thumbColor: Color = Color.Unspecified,
+        activeTrackColor: Color = Color.Unspecified,
+        inactiveTrackColor: Color = Color.Unspecified,
+    ): MovieSliderColors = MovieSliderColors(
+        thumbColor = thumbColor,
+        activeTrackColor = activeTrackColor,
+        inactiveTrackColor = inactiveTrackColor,
+    )
 }
 
 private fun calcFraction(start: Float, end: Float, currentValue: Float): Float =
     if (end == start) 0f
     else ((currentValue - start) / (end - start)).coerceIn(0f, 1f)
 
+
+internal class MovieSliderColors(
+    val thumbColor: Color,
+    val activeTrackColor: Color,
+    val inactiveTrackColor: Color,
+)
